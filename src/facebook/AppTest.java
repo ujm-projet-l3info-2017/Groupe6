0c321d24d8463af66c8ab28797351943d8695367
Chris <chrisjahim@free.fr>
2017-03-26 04:26:42 +0200

    feat: Ajout de test pour PageRazbot

--- src/facebook/AppTest.java
+++ src/facebook/AppTest.java
@@ -7,44 +7,20 @@ import facebook4j.FacebookFactory;
 import facebook4j.InboxResponseList;
 import facebook4j.Message;
 import facebook4j.PagableList;
+import facebook4j.RawAPIResponse;
 import facebook4j.Reading;
 import facebook4j.api.ConversationMethods;
 import facebook4j.auth.AccessToken;
+import facebook4j.internal.org.json.JSONException;
 
 public class AppTest
 {
 	public static void main(String args[])
 	{
-		//Récupèration les identifiants de la page facebook (settings.ini)
-		String appID = Token.getAppId();
-		String secretCode = Token.getSecretCode();
-		String pageToken = Token.getToken();
+		PageRazbot razbot = new PageRazbot();
 		
-		//Création de l'objet facebook
-		Facebook facebook = new FacebookFactory().getInstance();
+		System.out.println(razbot.existeMessagesNonLu());
 		
-		//Identification
-		facebook.setOAuthAppId(appID,secretCode);
-		AccessToken token = new AccessToken(pageToken);
-		facebook.setOAuthAccessToken(token);
-		
-		//Tests
-		try
-		{
-			InboxResponseList<Conversation> conversations;
-			conversations = facebook.getConversations();
-			Conversation conv1 = conversations.get(0);
-			
-			System.out.println(conv1);
-			//facebook.answerConversation("t_mid.1456287448561:f59d1b19d211d78343", "Test");
-		}
-		catch (IllegalStateException e)
-		{
-			e.printStackTrace();
-		}
-		catch (FacebookException e)
-		{
-			e.printStackTrace();
-		}
+		razbot.envoyerMessage("t_mid.1456287448561:f59d1b19d211d78343", "Test");
 	}
 }
