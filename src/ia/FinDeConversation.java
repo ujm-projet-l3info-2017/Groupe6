d1e917a4883a5bab9a6ee0fefb5247d6651a50c4
TGranjon <thomas.granjon@orange.fr>
2017-04-11 13:57:02 +0200

    doc: Déplacement des fichiers de vocabulaire dans un nouveau répertoire

---
+++ src/ia/FinDeConversation.java
@@ -0,0 +1,50 @@
+package facebook;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * Classe du vocabulaire de fin de conversation
+ * @author Thomas GRANJON
+ *
+ */
+public class FinDeConversation implements Vocabulaire{
+	
+	/**
+	 * Liste des expressions de fin de conversation
+	 */
+	static ArrayList<String> expressions;
+	
+	/**
+	 * Constructeur de la liste des expressions de fin de conversation
+	 */
+	public FinDeConversation(){
+		expressions.add("Au revoir");
+		expressions.add("Bye");
+		expressions.add("A la prochaine");
+		expressions.add("J'espere te revoir");
+	}
+	
+	/* (non-Javadoc)
+	 * @see facebook.Vocabulaire#getLength()
+	 */
+	public int getLength(){
+		int i=0;
+		while(expressions.get(i)!=null)
+		{
+			i++;
+		}
+		return i;
+	}
+
+	/* (non-Javadoc)
+	 * @see facebook.Vocabulaire#aleatoire()
+	 */
+	public String aleatoire() {
+
+		Random rand = new Random();
+		int nbAleatoire = rand.nextInt(((Vocabulaire) expressions).getLength()+1);
+		return expressions.get(nbAleatoire);
+	}
+
+}
---
+++ src/ia/Vocabulaire.java
@@ -0,0 +1,29 @@
+package facebook;
+
+import java.util.ArrayList;
+
+/**
+ * Interface des listes de vocabulaire
+ * @author Thomas GRANJON
+ *
+ */
+public interface Vocabulaire{
+	
+	/**
+	 * Cette liste contiendra les expressions relatives a chaque occasion
+	 */
+	static ArrayList<String> expressions = new ArrayList<String>();
+	
+
+	/**
+	 * Permet de connaitre la taille de la liste de vocabulaire
+	 * @return int : La taille de la liste
+	 */
+	public int getLength();
+	
+	/**
+	 * Permet de choisir une expression aleatoire
+	 * @return String : Une expression aleatoire parmi celles dans la liste expressions
+	 */
+	public String aleatoire();
+}
