1561500a59b07f1d01ca5695b1aa407fd63ed45f
Axel Sofonea <axel.sofo@hotmail.fr>
2017-04-24 17:34:52 +0200

    add: Conversation IA

---
+++ src/ia/ConversationIA.java
@@ -0,0 +1,20 @@
+package ia;
+
+public class ConversationIA {
+
+	/*
+	 * Initialisation de la conversation
+	 */
+	public ConversationIA() {
+		
+	}
+	
+	/*
+	 * Envoi d'un message
+	 */
+	public String ajoutMessage()
+	{
+		return null;
+	}
+
+}
