b44741e6c1176492f5c81b2cf50bf23f4058654d
Dimitri Bruyere <dimitri.bruyere@outlook.fr>
2017-05-12 23:01:00 +0200

    feat: fonction test api

--- src/allocine/FonctionsTest.java
+++ src/allocine/FonctionsTest.java
@@ -1,13 +1,16 @@
 package allocine;
 
+import com.moviejukebox.allocine.AllocineException;
+
 public class FonctionsTest
 {
 	/**
 	 *  Méthode de démo des fonctionnalités de l'api allociné
 	 * @param Le nom d'un film, virgule, le type de ressource demandé
 	 * @return La ressource demandé
+	 * @throws AllocineException 
 	 */
-	private String testApiAllocine(String message)
+	private String testApiAllocine(String message) throws AllocineException
 	{
 		String infos[] = message.split(",");
 		String resultat;
