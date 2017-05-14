6ca172120292fa797370b1ff25d87b4165234c6f
Chris <chrisjahim@free.fr>
2017-05-14 03:54:40 +0200

    feat: Ajout classe de test ConversationIA_V2

---
+++ src/ia/ConversationIA_V2.java
@@ -0,0 +1,37 @@
+package ia;
+
+public class ConversationIA_V2
+{
+	private String nom;
+	private Etape prochaineEtape;
+	
+	public ConversationIA_V2(String nom)
+	{
+		this.nom = nom;
+		prochaineEtape = Etape.DEBUT;
+		executerProchaineEtape();
+	}
+	
+	
+	public String executerProchaineEtape()
+	{
+		switch(prochaineEtape)
+		{
+		case DEBUT:
+			
+			break;
+		case SALUTATION:
+			
+			break;
+		case AVIS:
+			
+			break;
+		case PROPOSITION:
+			
+			break;
+		default:
+			return null;
+		}
+		return null;
+	}
+}
