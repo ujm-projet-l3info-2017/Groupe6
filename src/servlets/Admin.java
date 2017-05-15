646adf379f29293c892cf9e83152be8b35803ebc
Chris <chrisjahim@free.fr>
2017-05-15 03:12:53 +0200

    feat: Amélioration du système de connexion + ajout méthodes

--- src/servlets/Admin.java
+++ src/servlets/Admin.java
@@ -1,6 +1,7 @@
 package servlets;
 
 import java.io.IOException;
+import java.rmi.ServerException;
 import java.util.ArrayList;
 
 import javax.servlet.RequestDispatcher;
@@ -9,8 +10,13 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import chatbot.ChatBotThread;
+import ia.ConversationIA_V2;
 
 /**
  * Servlet implementation class Admin
@@ -18,6 +24,9 @@ import chatbot.ChatBotThread;
 @WebServlet("/Admin")
 public class Admin extends HttpServlet
 {
+	// Initialisation du log
+	static final Logger logger = LogManager.getLogger(Admin.class.getName());
+
 	private static final long serialVersionUID = 1L;
 	ChatBotThread chatbot = null;
 
@@ -42,13 +51,97 @@ public class Admin extends HttpServlet
 		}
 		else
 		{
-			//Vérification du code d'accès
-			if(request.getParameter("code")!=null)	
+			verificationConnexion(request,response);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		//Commande reçu
+		if(request.getParameter("selection")!=null)
+		{
+			gestionCommandeRecu(request,response);
+		}
+		else
+		{
+			//Recu connexion
+			if(request.getParameter("code")!=null)
 			{
-				if(request.getParameter("code").compareTo("lecode") == 0)
+				connexion(request,response);
+			}
+			else //Reçu POST de Facebook
+			{
+				traitementAlerteNouveauMessage(request,response);
+			}
+		}
+	}
+
+	private void gestionCommandeRecu(HttpServletRequest request, HttpServletResponse response)
+	{
+		logger.info("Servlet ADMIN","Recu POST commande serveur");
+		//Commande de lancement du serveur
+		if (request.getParameter("selection").compareTo("start") == 0)
+		{
+			lancementServeur();
+		}
+		else 
+		{
+			//Commande de fermeture du serveur
+			if (request.getParameter("selection").compareTo("stop") == 0 && chatbot != null)
+			{
+				fermetureServeur(); 
+				synchronized(chatbot)         
 				{
+					//Envoi d'un notify au thread qui gère l'IA
+					if(chatbot.getState()==Thread.State.WAITING)
+						chatbot.notify();
+				}
+			}
+		}
+		try
+		{
+			doGet(request,response);
+		}
+		catch (ServletException e)
+		{
+			logger.catching(e);
+		}
+		catch (IOException e)
+		{
+			logger.catching(e);
+		}
+	}
+
+	/**
+	 *  Vérification de la connexion dans la session
+	 * @param request
+	 * @param response
+	 */
+	private void verificationConnexion(HttpServletRequest request, HttpServletResponse response)
+	{
+		try
+		{
+			//Vérification du code d'accès
+			HttpSession session = request.getSession();
+			
+			String code = (String) session.getAttribute("code");
+			
+			if(code!=null)	
+			{
+				if(code.compareTo("lecode") == 0)
+				{
+					logger.info("Connexion réussi");
 					//Redirection vers la JSP d'administration
 					RequestDispatcher dispatcher = request.getRequestDispatcher("admin.jsp");
+					dispatcher.forward(request, response);
+				}
+				else
+				{
+					RequestDispatcher dispatcher = request.getRequestDispatcher("connexion_admin.jsp");
 					dispatcher.forward(request, response);
 				}
 			}
@@ -58,6 +151,14 @@ public class Admin extends HttpServlet
 				RequestDispatcher dispatcher = request.getRequestDispatcher("connexion_admin.jsp");
 				dispatcher.forward(request, response);
 			}
+		}
+		catch(IOException e)
+		{
+			logger.catching(e);
+		}
+		catch (ServletException e)
+		{
+			logger.catching(e);
 		}
 	}
 
@@ -86,50 +187,73 @@ public class Admin extends HttpServlet
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-	 *      response)
+	 *  Gère les alertes de nouveaux messages provenant de facebook
+	 * @param request
+	 * @param response
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	private void traitementAlerteNouveauMessage(HttpServletRequest request, HttpServletResponse response)
 	{
-		//Commande reçu
-		if(request.getParameter("selection")!=null)
+		if(chatbot!=null)
 		{
-			System.out.println("Recu POST commande serveur");
-			//Commande de lancement du serveur
-			if (request.getParameter("selection").compareTo("start") == 0)
+			logger.info("Servlet ADMIN","Reçu POST de Facebook");
+			synchronized(chatbot)
 			{
-				lancementServeur();
+				if(chatbot.getState()==Thread.State.WAITING)
+					chatbot.notify();
+				else
+					logger.info("Servlet ADMIN","Impossible de notify le thread car il n'est pas en wait");
 			}
-			else 
-			{
-				//Commande de fermeture du serveur
-				if (request.getParameter("selection").compareTo("stop") == 0 && chatbot != null)
-				{
-					fermetureServeur(); 
-					synchronized(chatbot)         
-					{
-						//Envoi d'un notify au thread qui gère l'IA
-						if(chatbot.getState()==Thread.State.WAITING)
-							chatbot.notify();
-					}
-				}
-			}
-			doGet(request,response);
 		}
-		else //Reçu POST de Facebook
+		try
 		{
-			if(chatbot!=null)
+			response.getWriter().append("HTTP 200 OK");
+		}
+		catch (IOException e)
+		{
+			logger.catching(e);
+		}
+	}
+
+	/**
+	 *  Etabli dans la session la connexion, provenant du formulaire de connexion_admin.jsp
+	 * @param request
+	 * @param response
+	 */
+	private void connexion(HttpServletRequest request, HttpServletResponse response)
+	{
+		HttpSession session = request.getSession();
+		
+		try
+		{
+			//Si le code n'a pas déjà été fixé
+			if(session.getAttribute("code")==null)
 			{
-				System.out.println("Reçu POST de Facebook");
-				synchronized(chatbot)
+				if(request.getParameter("code").compareTo("lecode")==0)
 				{
-					if(chatbot.getState()==Thread.State.WAITING)
-						chatbot.notify();
-					else
-						System.out.println("Impossible de notify le thread car il n'est pas en wait");
+					session.setAttribute("code", "lecode");
+					//Redirection vers la page
+					doGet(request, response);
+				}
+				else //Le code n'est pas bon
+				{
+					//Redirection vers la JSP d'authentification
+					logger.error("Connexion refusé : Erreur de mot de passe");
+					RequestDispatcher dispatcher = request.getRequestDispatcher("connexion_admin.jsp");
+					dispatcher.forward(request, response);
 				}
 			}
-			response.getWriter().append("HTTP 200 OK");
+			else //Le code est déjà fixé
+			{
+				verificationConnexion(request, response);
+			}
+		}
+		catch (ServletException e)
+		{
+			logger.catching(e);
+		}
+		catch (IOException e)
+		{
+			logger.catching(e);
 		}
 	}
 
@@ -146,7 +270,7 @@ public class Admin extends HttpServlet
 				chatbot.start();	//On le relance
 			}
 			else
-				System.out.println("Impossible de lancer le chatbot, le thread est déjà lancé");
+				logger.warn("Servlet ADMIN","Impossible de lancer le chatbot, le thread est déjà lancé");
 		}
 		else	//N'a jamais été lancé
 		{
